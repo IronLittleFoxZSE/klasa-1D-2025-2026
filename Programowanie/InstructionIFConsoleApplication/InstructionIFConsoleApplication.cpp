6c0856392b252a354b1562d21de9c6105f9af8a8
f <f@f.pl>
2025-10-17 13:18:47 +0200

    koniec lekcji

--- Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
+++ Programowanie/InstructionIFConsoleApplication/InstructionIFConsoleApplication.cpp
@@ -24,16 +24,58 @@ void task1()
 	if (number < 0)
 		std::cout << "Podana liczba jest ujemna\n";
 
-	if (number = 0)
+	if (number == 0)
 		std::cout << "Podana liczba nie jest dodatnia ani ujemna\n";
 
 	std::cout << "Koniec programu\n";
 }
 
+//Napisz program, który wyœwietli informacje czy liczba jest parzysta czy nieparzysta.
+void task2()
+{
+	int number;
+	std::cout << "Podaj liczbê ca³kowit¹:\n";
+	std::cin >> number;
+
+	int reminderOfDivision;
+	reminderOfDivision = number % 2;
+
+	if (reminderOfDivision == 0)
+		std::cout << "Podana liczba jest parzysta\n";
+
+	if (reminderOfDivision != 0) // == 1 
+		std::cout << "Podana liczba jest nieparzysta\n";
+
+	//--------------------------------------------------------
+	if (reminderOfDivision == 0)
+		std::cout << "Podana liczba jest parzysta\n";
+	else
+		std::cout << "Podana liczba jest nieparzysta\n";
+}
+
+//Napisz program, który wyœwietli informacje czy liczba jest z zakresu <1 ; 10).
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbê ca³kowit¹:\n";
+	std::cin >> number;
+
+	if (number >= 1)
+	{
+		if (number < 10)
+			std::cout << "Liczba jest w zakresie <1;10)\n";
+		else
+			std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	}
+	else
+	{
+		std::cout << "Liczba nie jest w zakresie <1;10)\n";
+	}
+}
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
-	task1();
+	task3();
 }
